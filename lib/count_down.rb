e771f29c947843520a3a9dcb5fc4080548dd1bd5
Victor Cordero <victordcordero@gmail.com>
2020-10-15 15:49:36 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,8 @@
 # Write your code here
 magic_exit_number = 12
-count = 0
-while count > 10 do
+count = 10
+while count < 0 do
+  break if count == magic_exit_number
   puts "#{count}"
   count = count + 1
 end
