acb0e4a4374be662dcb8365786325d3ddf2966b7
Victor Cordero <victordcordero@gmail.com>
2020-10-15 16:25:36 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,9 @@
 # Write your code here
-magic_exit_number = 12
+magic_exit_number = 10
 count = 10
 while count < 0 do
   break if count == magic_exit_number
   puts "#{count}"
   count = count + 1
+  puts "Happy New Year!"
 end
